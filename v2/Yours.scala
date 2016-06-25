158f03483314879a2adef3bf3232be82585204be
curoles <curoles@yahoo.com>
2016-06-25 00:12:27 -0700

    playing with scala parser

---
+++ v2/Yours.scala
@@ -0,0 +1,12 @@
+import yours._
+
+object Yours extends App {
+  //println("Hello, world!")
+  Console.println("Yours: " + (args mkString ", "))
+
+  val parser = new SentenceParser
+  val result = parser.parseAll(parser.sentence, "Hello Igor, number 1 super-hero.")
+  println("result:" + result)
+  if (result.successful) println(result.get)
+}
+
